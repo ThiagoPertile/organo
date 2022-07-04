d6809889eb393a5eade848a7852706deef6d630a
ThiagoPertile <thiagomcarmo@outlook.com>
2022-07-04 17:29:50 -0300

    Fiz Componente Colaborador (sem estilizacao)

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,17 @@
+import "./Colaborador.css";
+
+const Colaborador = (props) => {
+  return (
+    <div>
+      <div>
+        <img src="https://github.com/thiagoPertile.png" alt="Thiago Pertile" />
+      </div>
+      <div>
+        <h4>Thiago Pertile</h4>
+        <h5>Instrutor</h5>
+      </div>
+    </div>
+  );
+};
+
+export default Colaborador;
