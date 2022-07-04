7de95620ae358298bd4eb6bb541d20d48d3bca4b
ThiagoPertile <thiagomcarmo@outlook.com>
2022-07-04 16:10:51 -0300

    fiz dummy component (Time)

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
-import { useState } from 'react';
-import Banner from './componentes/Banner';
-import Formulario from './componentes/Formulario';
+import { useState } from "react";
+import Banner from "./componentes/Banner";
+import Formulario from "./componentes/Formulario";
+import Time from "./componentes/Time";
 
 function App() {
-
-  const [colaboradores, setColaboradores] = useState([])
+  const [colaboradores, setColaboradores] = useState([]);
 
   const aoNovoColaboradorAdicionado = (colaborador) => {
-    console.log(colaborador)
-    setColaboradores([...colaboradores, colaborador])
-  }
+    console.log(colaborador);
+    setColaboradores([...colaboradores, colaborador]);
+  };
 
   return (
     <div className="App">
       <Banner />
-      <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
+      <Formulario
+        aoColaboradorCadastrado={(colaborador) =>
+          aoNovoColaboradorAdicionado(colaborador)
+        }
+      />
+      <Time nome="Programação" />
+      <Time nome="Front-End" />
+      <Time nome="Data Science" />
     </div>
   );
 }
