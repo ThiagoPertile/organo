2a6e8dd62839ebba5edc605558c165b5ad9f8c88
ThiagoPertile <thiagomcarmo@outlook.com>
2022-07-04 10:16:49 -0300

    Fiz submisao do formulario

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,18 +1,14 @@
-import './CampoTexto.css'
-
+import "./CampoTexto.css";
 
 const CampoTexto = (props) => {
+  const placeholderModificado = `${props.placeholder}...`;
 
-    const placeholderModificado = `${props.placeholder}...`
+  return (
+    <div className="campo-texto">
+      <label>{props.label}</label>
+      <input required={props.obrigatorio} placeholder={placeholderModificado} />
+    </div>
+  );
+};
 
-    return (
-        <div className="campo-texto">
-            <label>
-                {props.label}
-            </label>
-            <input placeholder= {placeholderModificado}/>
-        </div>
-    )
-}
-
-export default CampoTexto
+export default CampoTexto;
--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,3 +1,4 @@
+import Botao from "../Botao";
 import CampoTexto from "../CampoTexto";
 import ListaSuspensa from "../LinstaSuspensa/insdex";
 import "./Formulario.css";
@@ -13,14 +14,28 @@ const Formulario = () => {
     "Inovacao e Gestao",
   ];
 
+  const oaSalvar = (evento) => {
+    evento.preventDefault();
+    console.log("Form foi submetido");
+  };
+
   return (
     <section className="formulario">
-      <form>
+      <form onSubmit={oaSalvar}>
         <h2>Preencha os dados para criar o card do colaborador</h2>
-        <CampoTexto label="Nome" placeholder="Digite seu nome" />
-        <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
+        <CampoTexto
+          obrigatorio={true}
+          label="Nome"
+          placeholder="Digite seu nome"
+        />
+        <CampoTexto
+          obrigatorio={true}
+          label="Cargo"
+          placeholder="Digite seu cargo"
+        />
         <CampoTexto label="Imagem" placeholder="Digite o enderço da imagem" />
         <ListaSuspensa label="times" itens={times} />
+        <Botao>Criar Card</Botao>
       </form>
     </section>
   );
