502d06675ec307a450c9181a52b2d60ff8080927
ThiagoPertile <thiagomcarmo@outlook.com>
2022-07-06 16:56:41 -0300

    Fiz mecaninsmo de reload de formulario

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -2,7 +2,7 @@ import Colaborador from "../Colaborador";
 import "./Time.css";
 
 const Time = (props) => {
-  return (
+  return props.colaboradores.length > 0 ? (
     <section className="time" style={{ backgroundColor: props.corSecundaria }}>
       <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
       <div className="colaboradores">
@@ -15,7 +15,27 @@ const Time = (props) => {
         ))}
       </div>
     </section>
+  ) : (
+    ""
   );
 };
+
+// ------------------------ PODE SE USAR A LOGICA DE TERNARIO TBM------------
+// const Time = (props) => {
+//   return props.colaboradores.length > 0 && (
+//     <section className="time" style={{ backgroundColor: props.corSecundaria }}>
+//       <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
+//       <div className="colaboradores">
+//         {props.colaboradores.map((colaborador) => (
+//           <Colaborador
+//             nome={colaborador.nome}
+//             cargo={colaborador.cargo}
+//             imagem={colaborador.imagem}
+//           />
+//         ))}
+//       </div>
+//     </section>
+//   )
+// };
 
 export default Time;
