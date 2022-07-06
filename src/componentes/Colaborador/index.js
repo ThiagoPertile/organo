4c8174bafe36a346a029d59362cf1357c761c353
ThiagoPertile <thiagomcarmo@outlook.com>
2022-07-06 15:33:06 -0300

    Fiz adicionamento de cards

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,14 +1,14 @@
 import "./Colaborador.css";
 
-const Colaborador = (props) => {
+const Colaborador = ({ nome, imagem, cargo }) => {
   return (
-    <div>
-      <div>
-        <img src="https://github.com/thiagoPertile.png" alt="Thiago Pertile" />
+    <div className="colaborador">
+      <div className="cabecalho">
+        <img src={imagem} alt={nome} />
       </div>
-      <div>
-        <h4>Thiago Pertile</h4>
-        <h5>Instrutor</h5>
+      <div className="rodape">
+        <h4>{nome}</h4>
+        <h5>{cargo}</h5>
       </div>
     </div>
   );
